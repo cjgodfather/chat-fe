644cd90fbef7b633b115da53dfcc95c5390a09ce
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-02-01 06:40:15 -0500

    connected to socket

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
+import io from "socket.io-client";
 
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const socket = io(`http://localhost:8000`);
+  socket.on("welcome back", () => {
+    console.log(`welcome back`);
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
