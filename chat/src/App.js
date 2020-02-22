0cfdaff039fb2a574e6171c5ab51a32da2a00cc6
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-02-21 21:27:08 -0500

    refactored chat and friendlist

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Chat from "./Chat";
 import io from "socket.io-client";
+import axios from "axios";
 import SearchPage from "./pages/SearchFriend";
 import FriendList from "./pages/FriendsList";
 
 import "./App.css";
 
 function App() {
+  const [friendId, setFriendId] = useState("");
+  const [friends, setFriends] = useState([]);
+
+  const getFriends = async () => {
+    const response = await axios.get(`http://localhost:5000/api/auth`);
+    setFriends(response.data.users);
+  };
+
+  const clickFriend = id => {
+    console.log(id);
+    setFriendId(id);
+  };
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
   const socket = io(`http://localhost:5000/chat`);
   socket.on("welcome back", () => {
     console.log(`welcome back`);
   });
+
   return (
     <div className="App">
-      <Chat socket={socket} />
+      <Chat socket={socket} friendId={friendId} />
       <SearchPage />
-      <FriendList />
+      <FriendList friends={friends} clickFriend={clickFriend} />
     </div>
   );
 }
--- chat/src/Chat.js
+++ chat/src/Chat.js
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from "react";
 
-const Chat = ({ socket }) => {
-  const [message, setMessage] = useState("");
+const Chat = ({ socket, friendId }) => {
+  const [newMessage, setNewMessage] = useState("");
   const [incomingMsg, setIncomingMsg] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
 
   const sendMessage = e => {
     e.preventDefault();
-    socket.emit("message", message);
+    socket.emit("message", newMessage);
   };
 
   socket.on("send message", msg => {
     setIncomingMsg(msg);
   });
 
+  useEffect(() => {
+    console.log(`get chat history with friendId`);
+  }, [friendId]);
+
   return (
     <div>
+      <div>{chatHistory}</div>
       <div>{incomingMsg}</div>
       <form onSubmit={sendMessage}>
         <input
           type="text"
-          value={message}
+          value={newMessage}
           name="message"
           onChange={e => {
-            setMessage(e.target.value);
+            setNewMessage(e.target.value);
           }}
         />
         <button>Send</button>
